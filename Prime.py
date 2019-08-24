1fc92b2806156a62fe711471b4f491b96eadf738
Aryan Kumar <arkumar.kum43@gmail.com>
2019-08-24 16:57:10 +0530

    commented fruits array

--- Prime.py
+++ Prime.py
@@ -1,12 +1,12 @@
 lower=10
 upper=20
-fruits = ["apple", "banana", "cherry"]
+#fruits = ["apple", "banana", "cherry"]
 for x in range(lower,upper):
 	for i in range(2,x):
 		if(x%i)==0:
 			break;
 		else:
-			print("%d Prime Number : \n",x)
+			print("Prime Number :%d\n",x)
 			  #print(x)
   #print(x)
   
