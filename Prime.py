25e4dff5e7ccadbbe440e202f4f494021a2afd17
Aryan Kumar <arkumar.kum43@gmail.com>
2019-08-24 16:21:24 +0530

    first version of code

---
+++ Prime.py
@@ -0,0 +1,13 @@
+lower=10
+upper=20
+fruits = ["apple", "banana", "cherry"]
+for x in range(lower,upper):
+	for i in range(2,x):
+		if(x%i)==0:
+			break;
+		else:
+			print("%d Prime Number : \n",x)
+			  #print(x)
+  #print(x)
+  
+  
